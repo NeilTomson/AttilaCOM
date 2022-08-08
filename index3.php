9b28b6f29b01fcaf243c84a295a92b1b4094268c
unknown <attilatoaso@yahoo.com>
2022-08-08 20:58:17 +0300

    ...

--- index3.php
+++ index3.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
-    <link rel="stylesheet" href="css/style2.css">
+    <link rel="stylesheet" href="style1.css">
 </head>
 <body>
-    <div class="container">
+    <div class="container-main">
         <nav>
             <div class="logo">
                 <a href="">
@@ -23,21 +23,159 @@
                     <a href="#" class="active"><i class="fa fa-home"></i>Home</a>
                 </li>
                 <li class="navlink">
-                    <a href="#"><i class="fa fa-user"></i>Projektet</a>
+                    <a href="#"><i class="fa fa-user"></i>Project</a>
                 </li>
                 <li class="navlink">
-                    <a href="#"><i class="fa fa-list"></i>Rolam</a>
+                    <a href="#"><i class="fa fa-list"></i>About me</a>
                 </li>
                 <li class="navlink">
-                    <a href="#"><i class="fa fa-comments"></i>Kontaktok</a>
+                    <a href="#"><i class="fa fa-comments"></i>Contact</a>
                 </li>
             </ul>
         </nav>
-        <main>
-            <section class="home section">
-                home
+        <main class="main-contant">
+            <section class="home section hidden">
+                <div class="contant">
+                    <div class="row">
+                        <div class="home-info padding ">
+                            <h1 class="helo">Hello my name is <span class="name">Attila</span></h1>
+                            <h3 class="myprofession">I am a <span class="Typing">fullstack developer</span></h3>
+                            <p>I am a beginner fullstack developer with a great desire to create something useful for the world</p>
+                            <a href="#" class="btn">More about me</a>
+                        </div>
+                        <div class="home-img padding">
+                            <img src="img/me.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
             </section>
-
+            <section class="about section">
+                <div class="container about">
+                    <div class="row">
+                        <div class="section-title padding">
+                            <h1>About us</h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="about-text padding">
+                            <h3>I am a <span class="Typing">fullstack developer</span></h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae vero sint, natus, consequatur mollitia voluptatibus dicta, repellat beatae laborum adipisci modi voluptates. Officiis perspiciatis illo necessitatibus, aperiam corrupti asperiores iure.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="personal-info padding">
+                            <div class="row">
+                                <div class="info-item padding">
+                                    <p>Birthday: <span>1999.08.30</span> </p>
+                                </div>
+                                <div class="info-item padding">
+                                    <p>Age: <span>22</span> </p>
+                                </div>
+                                <div class="info-item padding">
+                                    <p>Website: <span>.....</span> </p>
+                                </div>
+                                <div class="info-item padding">
+                                    <p>Email: <span>info@yahoo.com</span> </p>
+                                </div>
+                                <div class="info-item padding">
+                                    <p>City: <span>Odorhei secuiesc /Székelyudvarhely</span> </p>
+                                </div>
+                                <div class="info-item padding">
+                                    <p>Country: <span>Romania</span> </p>
+                                </div>
+                                <div class="button padding">
+                                    <a href="" class="btn hire me">Contact me</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="skills padding">
+                            <div class="row">
+                                <div class="skill-item padding">
+                                    <h5>HTML</h5>
+                                    <div class="progress">
+                                        <div class="progress-in" style="width: 76%;"></div>
+                                        <div class="skill-percent">76%</div>
+                                    </div>
+                                </div>
+                                <div class="skill-item padding">
+                                    <h5>CSS</h5>
+                                    <div class="progress">
+                                        <div class="progress-in" style="width: 76%;"></div>
+                                        <div class="skill-percent">76%</div>
+                                    </div>
+                                </div>
+                                <div class="skill-item padding">
+                                    <h5>Javascript</h5>
+                                    <div class="progress">
+                                        <div class="progress-in" style="width: 80%;"></div>
+                                        <div class="skill-percent">76%</div>
+                                    </div>
+                                </div>
+                                <div class="skill-item padding">
+                                    <h5>PHP</h5>
+                                    <div class="progress">
+                                        <div class="progress-in" style="width: 16%;"></div>
+                                        <div class="skill-percent">76%</div>
+                                    </div>
+                                </div>
+                                <div class="skill-item padding">
+                                    <h5>MySQL</h5>
+                                    <div class="progress">
+                                        <div class="progress-in" style="width: 80%;"></div>
+                                        <div class="skill-percent">76%</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--ide irjak majd be egy részt az egyetemről-->
+                </div>
+            </section>
+            <section class="portolfolio section">
+                <div class="container">
+                    <div class="row">
+                        <div class="section-title padding">
+                            <h1>Portofolio</h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="portofolio-head padding">
+                            <h2>My projects:</h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="portofolio-item padding">
+                            <div class="portofolio-item-inner">
+                                <div class="portofolio-img">
+                                    <img src="" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="portofolio-item padding">
+                            <div class="portofolio-item-inner">
+                                <div class="portofolio-img">
+                                    <img src="" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="portofolio-item padding">
+                            <div class="portofolio-item-inner">
+                                <div class="portofolio-img">
+                                    <img src="" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="portofolio-item padding">
+                            <div class="portofolio-item-inner">
+                                <div class="portofolio-img">
+                                    <img src="" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
         </main>
     </div>
 </body>
